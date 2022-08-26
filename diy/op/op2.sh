43819f939cf348f551b48af4ac08b03ff23ca009
lffw001 <332930997@qq.com>
2022-08-27 00:10:56 +0800

    Update op2.sh

--- diy/op/op2.sh
+++ diy/op/op2.sh
@@ -27,7 +27,7 @@ cp -af ./package/add/luci-app-filetransfer/po/zh-cn  ./package/add/luci-app-file
 rm -rf package/feeds/xiangfeidexiaohuo/luci-app-turboacc
 
 ##配置IP
-#sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.1/g' package/base-files/files/bin/config_generate
 
 ##取消bootstrap为默认主题
 sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
