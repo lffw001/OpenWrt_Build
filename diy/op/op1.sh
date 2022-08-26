196acbc0a011ae53fba017175db26e98b5d3d76d
lffw001 <332930997@qq.com>
2022-08-27 07:27:40 +0800

    Update op1.sh

--- diy/op/op1.sh
+++ diy/op/op1.sh
@@ -5,4 +5,3 @@
 #=================================================
 ##添加自己的插件库
 sed -i "1isrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages\n" feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
